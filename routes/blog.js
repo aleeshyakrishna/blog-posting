15923de296230c810150f42a3a29b114a110ac92
Aleeshya Krishna MV <aleeshyakrishnamv@Aleeshyas-MacBook-Air.local>
2025-02-23 14:49:40 +0530

    validation

--- routes/blog.js
+++ routes/blog.js
@@ -9,9 +9,8 @@ import multer from 'multer';
 const storage = multer.memoryStorage();
 const upload = multer({ storage: storage });
 
-// Route for creating a blog with multiple image uploads
-router.post('/create-blog', auth, upload.array('image', 10), blogController.createBlog); 
 
+router.post('/create-blog', auth, upload.array('image', 10), blogController.createBlog); 
 router.get('/my-blogs', auth ,blogController.getBlogs);
 router.get('/my-blog/:id', blogController.getBlogById);
 router.put('/update/:id', auth, blogController.updateBlog);
